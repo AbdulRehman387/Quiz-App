ca3c293ea830aa1e5d66d42dc6420272ee8b1e56
Mounas82 <abdulrehman03082007@gmail.com>
2024-05-25 23:35:36 +0500

    Quiz App added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,121 @@
-import Image from "next/image";
-
+"use client"
+import * as questions from './questions.json';
+import { useState } from "react";
+type questionType = {
+  question: string,
+  A: string,
+  B: string,
+  C: string,
+  D: string,
+  answer: string
+}
+let index: number = 0
+let counter: number = 0
 export default function Home() {
+  const [display, setDisplay] = useState<boolean>(false)
+  const [color1, setColor1] = useState<string>("#E5E7EB")
+  const [color2, setColor2] = useState<string>("#E5E7EB")
+  const [color3, setColor3] = useState<string>("#E5E7EB")
+  const [color4, setColor4] = useState<string>("#E5E7EB")
+  const [question, setQuestion] = useState<questionType>(questions[0])
+  const onClickHandler = () => {
+    setColor1("#E5E7EB")
+    setColor2("#E5E7EB")
+    setColor3("#E5E7EB")
+    setColor4("#E5E7EB")
+    if ((
+      color1 === "red" ||
+      color1 === "green" ||
+      color2 === "red" ||
+      color2 === "green" ||
+      color3 === "red" ||
+      color3 === "green" ||
+      color4 === "red" ||
+      color4 === "green"
+    )) {
+      if (index !== questions.length - 1) {
+        index++;
+        setQuestion(questions[index])
+      }
+      else {
+        setDisplay(true);
+      }
+    }
+    else{
+      alert("Please Select Atleast One Option!")
+    }
+  }
+  const onClickButtonHandler = (option: string) => {
+    if (!(
+      color1 === "red" ||
+      color1 === "green" ||
+      color2 === "red" ||
+      color2 === "green" ||
+      color3 === "red" ||
+      color3 === "green" ||
+      color4 === "red" ||
+      color4 === "green"
+    )) {
+      if (option === "A") {
+        if (option === question.answer) {
+          setColor1("green")
+          counter++
+        }
+        else {
+          setColor1("red")
+        }
+      }
+      else if (option === "B") {
+        if (option === question.answer) {
+          setColor2("green")
+          counter++
+        }
+        else {
+          setColor2("red")
+        }
+      }
+      else if (option === "C") {
+        if (option === question.answer) {
+          setColor3("green")
+          counter++
+        }
+        else {
+          setColor3("red")
+        }
+      }
+      else if (option === "D") {
+        if (option === question.answer) {
+          setColor4("green")
+          counter++
+        }
+        else {
+          setColor4("red")
+        }
+      }
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div style={{ display: display ? "none" : "flex" }} className="flex-col w-[100vw] h-[100vh] justify-center items-center gap-y-16 mobile:gap-y-10">
+        <h1 className="text-6xl font-extrabold text-blue-900">Quiz</h1>
+        <div className="w-[60%] flex flex-col justify-center items-center px-10 py-10 gap-y-10 mobile:w-full">
+          <h1 className="text-xl font-bold mobile:text-lg mobile:font-semibold text-blue-900">Q{index+1}: {question.question}</h1>
+          <button style={{ backgroundColor: color1 }} onClick={() => onClickButtonHandler("A")} className={"w-full min-h-14 h-auto rounded-3xl text-lg font-bold px-2 py-2 mobile:text-base mobile:font-semibold hover:scale-105 transition-all ease-out duration-300 shadow-xl"}>
+            {question.A}
+          </button>
+          <button style={{ backgroundColor: color2 }} onClick={() => onClickButtonHandler("B")} className={"w-full min-h-14 h-auto rounded-3xl text-lg font-bold px-2 py-2 mobile:text-base mobile:font-semibold hover:scale-105 transition-all ease-out duration-300 shadow-xl"}>
+            {question.B}
+          </button>
+          <button style={{ backgroundColor: color3 }} onClick={() => onClickButtonHandler("C")} className={"w-full min-h-14 h-auto rounded-3xl text-lg font-bold px-2 py-2 mobile:text-base mobile:font-semibold hover:scale-105 transition-all ease-out duration-300 shadow-xl"}>
+            {question.C}
+          </button>
+          <button style={{ backgroundColor: color4 }} onClick={() => onClickButtonHandler("D")} className={"w-full min-h-14 h-auto rounded-3xl text-lg font-bold px-2 py-2 mobile:text-base mobile:font-semibold hover:scale-105 transition-all ease-out duration-300 shadow-xl"}>
+            {question.D}
+          </button>
+          <button onClick={onClickHandler} className="w-60 h-14 rounded-3xl bg-blue-900 text-white text-lg font-bold mobile:w-40            hover:scale-105 transition-all ease-out duration-300">Next</button>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div style={{ display: display ? "" : "none" }} className="text-8xl text-blue-900 font-extrabold flex justify-center items-center w-[100vw] h-[100vh] mobile:text-5xl mobile:text-center"><h1>Your Score is {counter}/{questions.length}!</h1></div>
     </main>
   );
 }
